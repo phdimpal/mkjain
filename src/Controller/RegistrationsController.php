b6d14498fdfdee10e2d8f79243276373b4dbf72e
rohitkumarjoshi <rohitkumarjoshi43@gmail.com>
2018-10-30 14:10:00 +0530

    login screen completed

--- src/Controller/RegistrationsController.php
+++ src/Controller/RegistrationsController.php
@@ -1,7 +1,15 @@
 <?php
 namespace App\Controller;
-
+use Cake\View\View;
+use Cake\View\ViewBuilder;
+use Cake\Filesystem\Folder;
+use Cake\Filesystem\File;
 use App\Controller\AppController;
+use Cake\ORM\TableRegistry;
+use Cake\Validation\Validation;
+use Cake\Routing\Router;
+use Cake\Mailer\Email;
+use Cake\Event\Event;
 
 /**
  * Registrations Controller
@@ -18,14 +26,40 @@ class RegistrationsController extends AppController
      *
      * @return \Cake\Http\Response|void
      */
+	 
+	 public function initialize()
+	{
+		parent::initialize();
+		$this->Auth->allow(['logout','login']);
+		
+	}
+	 
 	public function login(){
 		$this->viewBuilder()->layout('login_layout');
+		if ($this->request->is('post')) {
+			
+			$user = $this->Auth->identify();
+			if($user){
+				$this->Auth->setUser($user);
+				return $this->redirect(['action' => 'index']);
+				
+			}
+			$this->Flash->error('Your username or password is incorrect.');
+			
+		}
 	} 
+	
+	public function logout()
+	{
+		
+		$this->Flash->success('You are now logged out.');
+		return $this->redirect($this->Auth->logout());
+	}
 	
     public function index()
     {
         $this->paginate = [
-            'contain' => ['MasterRoles', 'MasterClasses', 'MasterSections', 'MasterMedia']
+            'contain' => ['MasterRoles', 'MasterClasses', 'MasterSections', 'MasterMediums']
         ];
         $registrations = $this->paginate($this->Registrations);
 
