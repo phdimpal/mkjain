659d25a1f9ed42c1533a1c711032f99eb774205d
rohitkumarjoshi <rohitkumarjoshi43@gmail.com>
2018-11-15 12:56:56 +0530

    leave reject notification completed

--- src/Controller/LeavesController.php
+++ src/Controller/LeavesController.php
@@ -192,7 +192,75 @@ class LeavesController extends AppController
         $this->request->allowMethod(['post', 'delete']);
         $leave = $this->Leaves->get($id);
 		$leave->status='Rejected';
+		$registration_id=$leave->registration_id;
+		
         if ($this->Leaves->save($leave)) {
+			
+			// Notifications Code Start	
+						$Registrationsnews=$this->Leaves->Registrations->find()->where(['Registrations.is_deleted'=>0,'Registrations.id'=>$registration_id,'Registrations.device_token !='=>'']);
+						date_default_timezone_set("Asia/Calcutta");
+						foreach($Registrationsnews as $Registrationsnew){
+							
+							$reg_id=$Registrationsnew->id;
+							$device_token=$Registrationsnew->device_token;
+							
+							$tokens = array($device_token);
+							$random=(string)mt_rand(1000,9999);
+							$header = [
+							'Content-Type:application/json',
+							'Authorization: Key=AAAAMDhcGSU:APA91bGGXZ2FClcRw5lmRvE76x5OHKrm2wqk8Xy5hBBYu0OYPjXrP5c7NJlR8yeYZxWBmC5DwFILj3Tzw7pqZ_zzPrSmI4E2_2j22QVrm4jnUgY6c6SLldZH7eSjaD0CHqryqJqz_oFR'
+							];
+
+							$msg = [
+							'title'=> 'Leave Application',
+							'message' => 'Your Leave Application has been rejected',
+							'image' => '',
+							'link' => 'mkjain://Leaves?id='.$id,
+							'notification_id'    => $random,
+							];
+							
+							$payload = array(
+							'registration_ids' => $tokens,
+							'data' => $msg
+							);
+
+							$curl = curl_init();
+							curl_setopt_array($curl, array(
+							CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+							CURLOPT_RETURNTRANSFER => true,
+							CURLOPT_CUSTOMREQUEST => "POST",
+							CURLOPT_POSTFIELDS => json_encode($payload),
+							CURLOPT_HTTPHEADER => $header
+							));
+							$response = curl_exec($curl);
+							$err = curl_error($curl);
+							curl_close($curl);
+							$final_result=json_decode($response);
+							$sms_flag=$final_result->success; 	
+							if ($err) {
+							//echo "cURL Error #:" . $err;
+							} else {
+							//$response;
+							}	
+							if($sms_flag==1){
+								
+								$Notifications=$this->Leaves->Registrations->Notifications->newEntity();
+								$Notifications->title='Leave Application';
+								$Notifications->message='Your Leave Application has been rejected';
+								$Notifications->notify_date=date("Y-m-d");
+								$Notifications->notify_time=date("h:i A"); 
+								$Notifications->created_by=0; 
+								$Notifications->registration_id=$reg_id; 
+								$Notifications->notify_link='mkjain://Leaves?id='.$id; 
+								$this->Leaves->Registrations->Notifications->save($Notifications);
+							
+							}
+						}
+					//End Notification Code		
+			
+			
+			
+			
             $this->Flash->success(__('The leave has been rejected.'));
         } else {
             $this->Flash->error(__('The leave could not be rejected. Please, try again.'));
